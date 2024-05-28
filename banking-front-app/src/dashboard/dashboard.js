cbb6f7b89759385cbb1c12da0472831464e20bf5
Dmytro Kiperman <8boyss@ukr.net>
2024-05-29 01:14:07 +0200

    bankingapp :  create components

---
+++ banking-front-app/src/dashboard/dashboard.js
@@ -0,0 +1,7 @@
+export function Dashboard() {
+    return (
+        <>
+          This is Dashboard
+        </>
+    )
+}
---
+++ banking-front-app/src/new-customer/new-customer.js
@@ -0,0 +1,7 @@
+export function NewCustomer() {
+    return (
+        <>
+          This is NewCustomer
+        </>
+    )
+}
