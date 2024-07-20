5b2e0fd4499a5f3db7e7eaeedbd59cd09fdf6cef
Dmytro Kiperman <8boyss@ukr.net>
2024-07-20 13:17:57 +0200

    upd

--- my-app/src/deposit/deposit.js
+++ my-app/src/deposit/deposit.js
@@ -1,10 +1,13 @@
 import styles from './deposit.module.css'
 
 export function Deposit() {
+
+    const onDeposit = () => {}
+
     return (
       <div className={styles.depCont}>
       <h1>Deposit Amount</h1>
-      <form >
+      <form onSubmit={onDeposit}>
         <input type="number" placeholder="Account Id" name="acId"/>
         <input type="text" placeholder="Amount" name="amount"/>
         <input type="submit" value="Deposit" />
