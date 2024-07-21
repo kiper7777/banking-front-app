72e611e42793c0e0c294f8e1feedec95781cc871
Dmytro Kiperman <8boyss@ukr.net>
2024-07-21 16:52:43 +0200

    upd

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Dashboard } from './dashboard/dashboard';
+import { NewCustomer } from './new-customer/new-customer';
+import { Deposit } from './deposit/deposit';
+import { Transactions } from './transactions/transactions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Dashboard />} />
+      <Route path='/create' element={<NewCustomer />} />
+      <Route path='/deposit' element={<Deposit />} />
+      <Route path='/transactions' element={<Transactions />} />
+    </Routes>
+  </BrowserRouter>
+);
 
-export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
