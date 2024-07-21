1adaafd43968342a833c0480247a3a0a525c3194
Dmytro Kiperman <8boyss@ukr.net>
2024-07-21 16:45:11 +0200

    upd

--- backend/src/handlers.js
+++ backend/src/handlers.js
@@ -1,9 +1,9 @@
 // src/handlers.js
 
 let accounts = {};  // In-memory storage for accounts
+let transactions = [];  // In-memory storage for transactions
 let nextAccountId = 1;
 
-// Function to create a new account
 function createNewAccount(data, callback) {
   const { name } = data;
   const accountId = nextAccountId++;
@@ -11,23 +11,23 @@ function createNewAccount(data, callback) {
   callback(`Account created for ${name} with ID ${accountId}`);
 }
 
-// Function to deposit money into an account
 function deposit(data, callback) {
   const { accountId, amount } = data;
   if (accounts[accountId]) {
     accounts[accountId].balance += amount;
+    transactions.push({ type: 'deposit', accountId, amount, date: new Date() });
     callback(`Deposited $${amount} to account ID ${accountId}`);
   } else {
     callback(`Account ID ${accountId} not found`);
   }
 }
 
-// Function to withdraw money from an account
 function withdraw(data, callback) {
   const { accountId, amount } = data;
   if (accounts[accountId]) {
     if (accounts[accountId].balance >= amount) {
       accounts[accountId].balance -= amount;
+      transactions.push({ type: 'withdraw', accountId, amount, date: new Date() });
       callback(`Withdrew $${amount} from account ID ${accountId}`);
     } else {
       callback(`Insufficient funds in account ID ${accountId}`);
@@ -37,7 +37,6 @@ function withdraw(data, callback) {
   }
 }
 
-// Function to check the balance of an account
 function balance(data, callback) {
   const { accountId } = data;
   if (accounts[accountId]) {
@@ -47,13 +46,13 @@ function balance(data, callback) {
   }
 }
 
-// Function to transfer money between accounts
 function transfer(data, callback) {
   const { fromAccountId, toAccountId, amount } = data;
   if (accounts[fromAccountId] && accounts[toAccountId]) {
     if (accounts[fromAccountId].balance >= amount) {
       accounts[fromAccountId].balance -= amount;
       accounts[toAccountId].balance += amount;
+      transactions.push({ type: 'transfer', fromAccountId, toAccountId, amount, date: new Date() });
       callback(`Transferred $${amount} from account ID ${fromAccountId} to account ID ${toAccountId}`);
     } else {
       callback(`Insufficient funds in account ID ${fromAccountId}`);
@@ -63,7 +62,8 @@ function transfer(data, callback) {
   }
 }
 
-module.exports = { createNewAccount, deposit, withdraw, balance, transfer };
+function getTransactions(callback) {
+  callback(transactions);
+}
 
-
-
+module.exports = { createNewAccount, deposit, withdraw, balance, transfer, getTransactions };
